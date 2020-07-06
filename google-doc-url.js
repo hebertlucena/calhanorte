b9a9d9967fbababa3669b51e08e53f85be1d1dc8
hebertlucena <67700370+hebertlucena@users.noreply.github.com>
2020-07-06 20:55:14 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1fJ9faRzD91wcxZjoNOWy-QyOdP4z659cSikpWo54Nw0/edit?usp=sharing';
